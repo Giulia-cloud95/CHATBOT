ba0df7d8ecdf5f6cd2501be5c94c7e4b4910ba2b
Giulia-cloud95 <giuliacolletta24@outlook.it>
2026-03-05 20:51:37 +0100

    Update CHATBOT.py

--- CHATBOT.py
+++ CHATBOT.py
@@ -104,7 +104,7 @@ if domanda:
       # https://platform.openai.com/docs/models/compare
 
 #Prompt: deve avere {context} (per i documenti) e {question}
-    prompt = ChatPromptTemplate.from_messages([
+  prompt = ChatPromptTemplate.from_messages([
 ("system", "Sei un assistente che risponde solo in base al contesto fornito."),
 #("human", "Domanda: {question}\n\nContesto:\n{context}")])  
   
