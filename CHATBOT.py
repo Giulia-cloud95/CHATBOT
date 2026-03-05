52f9360d5da0be71dce060a98fb48dfd16962f9f
Giulia-cloud95 <giuliacolletta24@outlook.it>
2026-03-05 16:39:30 +0100

    Update CHATBOT.py

--- CHATBOT.py
+++ CHATBOT.py
@@ -50,7 +50,7 @@ if file is not None:
   testo_letto = PdfReader(file)
   testo = ""
   for pagina in testo_letto.pages:
-  testo = testo + pagina.extract_text()
+    testo = testo + pagina.extract_text()
     st.write(testo)
 
     # Usiamo il text splitter di Langchain
