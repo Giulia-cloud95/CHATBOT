1f7a5aa06fb79d7ec9045922a838db1c881d0e1a
Giulia-cloud95 <giuliacolletta24@outlook.it>
2026-03-05 20:23:51 +0100

    Update CHATBOT.py

--- CHATBOT.py
+++ CHATBOT.py
@@ -105,8 +105,8 @@ if domanda:
 
 #Prompt: deve avere {context} (per i documenti) e {question}
     #prompt = ChatPromptTemplate.from_messages([
-    ("system", "Sei un assistente che risponde solo in base al contesto fornito."),
-    ("human", "Domanda: {question}\n\nContesto:\n{context}")])  
+  ("system", "Sei un assistente che risponde solo in base al contesto fornito."),
+  ("human", "Domanda: {question}\n\nContesto:\n{context}")])  
   
     # Nuova doc chain che sostituisce load_qa_chain
   chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
