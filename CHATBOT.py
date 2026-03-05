6703d021336a64e9b2ed10999babe328b0fc3ab7
Giulia-cloud95 <giuliacolletta24@outlook.it>
2026-03-05 20:21:08 +0100

    Update CHATBOT.py

--- CHATBOT.py
+++ CHATBOT.py
@@ -113,7 +113,7 @@ if domanda:
             
 #Output
           # Chain: prendi la domanda, individua i frammenti rilevanti,
-  passali all'LLM, genera la risposta
+  #passali all'LLM, genera la risposta
 risposta = chain.invoke({"input_documentes": rilevanti, "question": domanda})
 st.write(risposta)
 
